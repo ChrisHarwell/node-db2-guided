4b482b15d532de8de9c3d70abbd638c6d445a988
Chris Harwell <chrisharwell72@gmail.com>
2020-08-18 22:19:36 -0400

    Guided project complete

---
+++ data/migrations/20200818210322_fruits-schema.js
@@ -0,0 +1,12 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable("fruits", (tbl) => {
+    tbl.increments();
+    tbl.text("name", 128).unique().notNullable();
+    tbl.float("avgWeightOz").notNullable();
+    tbl.boolean("delicious").defaultTo(false);
+  });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists("fruits");
+};
